6d7d51841c0627ba091fb1132bc5e6a61c6b07e2
Brzusko <wojciech.brzuszkiewicz@hotmail.com>
2025-01-24 18:18:18 +0100

    [Code] Simple player spawner

--- scripts/spawners/player_spawner.gd
+++ scripts/spawners/player_spawner.gd
@@ -1,1 +1,19 @@
-extends Node3D
+extends Node3D;
+class_name PlayerSpawner;
+
+@export var player_scene: PackedScene = null;
+@export var spawn_point: Marker3D = null;
+
+var player_node: Node3D = null;
+
+signal player_spawned(player: Node3D);
+
+func spawn_player() -> PlayerSpawner:
+	player_node = player_scene.instantiate() as Node3D;
+	
+	if player_node == null:
+		printerr("Could not spawn player!!");
+	
+	# Moźna tutaj dodać jakas sekwencje spawnowania
+	get_tree().create_timer(0.5).timeout.connect(func(): player_spawned.emit(player_node));
+	return self;
