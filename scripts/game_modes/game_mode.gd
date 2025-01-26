6008837ffd83713701879063bb1a37717305037e
Brzusko <wojciech.brzuszkiewicz@hotmail.com>
2025-01-26 17:55:31 +0100

    [Assets] Audio and Loading ui

--- scripts/game_modes/game_mode.gd
+++ scripts/game_modes/game_mode.gd
@@ -19,11 +19,20 @@ func _start_game() -> void:
 	game_map.add_child(player_entity);
 	input_provider.input_chaged.connect(_on_input_changed);
 	
-	for to_disable: ProcessDisable in GameData.node_disablers.values():
-		to_disable.disable();
+	pause_game();
 	
 	await loading_ui._start_closing().close;
 	loading_ui.hide();
+	
+	unpause_game();
+
+func pause_game() -> void:
+	for to_disable: ProcessDisable in GameData.node_disablers.values():
+		to_disable.disable();
+
+func unpause_game() -> void:
+	for to_disable: ProcessDisable in GameData.node_disablers.values():
+		to_disable.enable();
 	
 func _on_input_changed(input_data: InputData) -> void:
 	player_entity.handle_input(input_data);
