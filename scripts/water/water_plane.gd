d33d86a2da41d00c2796b5ba80a06678e5424fc2
Brzusko <wojciech.brzuszkiewicz@hotmail.com>
2025-01-26 12:29:51 +0100

    [Code] Water plane, branch inserting

---
+++ scripts/water/water_plane.gd
@@ -0,0 +1,25 @@
+extends Node
+class_name WaterPlane;
+
+@export var water_plane: Node3D;
+@export var starting_height: Marker3D;
+
+var starting_water_hight: float;
+var current_tween: Tween;
+
+func _ready() -> void:
+	starting_water_hight = water_plane.global_position.y;
+	water_plane.global_position.y = starting_height.global_position.y;
+
+func _on_dam_progress(current: int, total: int) -> void:
+	if current_tween != null && current_tween.is_running():
+		current_tween.stop();
+		
+	
+	var starting_height: float = starting_height.global_position.y;
+	var target_height: float = starting_water_hight;
+	var normalized_value: float = float(current) / float(total);
+	var desired_height: float = lerpf(starting_height, target_height, normalized_value);
+	
+	# TODO: add water animation
+	water_plane.global_position.y = desired_height;
